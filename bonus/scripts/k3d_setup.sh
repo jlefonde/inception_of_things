751588c64d039971ecf37f490d174b26e8bb5f52
jlefonde <joris.lefondeur@protonmail.com>
2025-04-12 00:07:16 +0200

    Modified bonus

--- bonus/scripts/k3d_setup.sh
+++ bonus/scripts/k3d_setup.sh
@@ -14,8 +14,25 @@ k3d cluster create iot
 
 export KUBECONFIG=$(k3d kubeconfig write iot)
 
+kubectl wait -n kube-system --for=create svc traefik --timeout=120s
+
+EXTERNAL_IP=""
+while [ -z "$EXTERNAL_IP" ]; do
+  echo "Waiting for external IP"
+  EXTERNAL_IP=$(kubectl get -n kube-system svc traefik --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  [ -z "$EXTERNAL_IP" ] && sleep 1
+done
+
+OLD_EXTERNAL_IP=$(grep gitlab.app.com /etc/hosts | awk '{print $1}')
+
+if [ -z "$OLD_EXTERNAL_IP" ]; then
+  sudo echo "$EXTERNAL_IP    gitlab.app.com" >> /etc/hosts
+else
+  sudo sed -i 's/'$OLD_EXTERNAL_IP'/'$EXTERNAL_IP'/g' /etc/hosts
+fi
+
 echo -e "${BOLD_GREEN}[2/11] Installing gitlab${RESET}"
-helm upgrade -n gitlab --install gitlab gitlab/gitlab --create-namespace --timeout 600s -f ../confs/values_k3d.yml
+helm upgrade -n gitlab --install gitlab gitlab/gitlab --create-namespace --timeout 600s --set global.hosts.externalIP=$EXTERNAL_IP -f ../confs/values_k3d.yml
 
 echo -e "${BOLD_GREEN}[3/11] Waiting for gitlab-gitlab-initial-root-password to be created...${RESET}"
 kubectl wait -n gitlab --for=create secret gitlab-gitlab-initial-root-password --timeout=120s
@@ -37,7 +54,7 @@ while [[ $SECONDS -lt $end ]]; do
 done || (echo -e "${BOLD_RED} Gitlab is not accessible${RESET}" && exit 1)
 
 echo -e "${BOLD_GREEN}[6/11] Creating GitLab project${RESET}"
-GITLAB_TOKEN=$(curl -s -X POST "http://gitlab.app.com/oauth/token" \
+export GITLAB_TOKEN=$(curl -s -X POST "http://gitlab.app.com/oauth/token" \
   -d "grant_type=password&username=root&password=$GITLAB_PASSWORD" | jq -r .access_token)
 
 curl -s -X POST "http://gitlab.app.com/api/v4/projects" \
@@ -45,7 +62,7 @@ curl -s -X POST "http://gitlab.app.com/api/v4/projects" \
   -H "Content-Type: application/json" \
   -d "{
     \"name\": \"inception_of_things_conf\",
-    \"visibility\": \"private\"
+    \"visibility\": \"public\"
   }"
 
 echo -e "${BOLD_GREEN}[7/11] Generating ssh key${RESET}"
@@ -64,7 +81,7 @@ echo -e "${BOLD_GREEN}[9/11] Cloning gitlab project${RESET}"
 git clone http://root:$GITLAB_PASSWORD@gitlab.app.com/root/inception_of_things_conf.git ../../../iot_conf
 
 echo -e "${BOLD_GREEN}[10/11] Copying config files${RESET}"
-cp -R ../confs/repo/ ../../../iot_conf/
+cp -R ../confs/repo/* ../../../iot_conf/
 
 cd ../../../iot_conf
 git config --local user.email "root@gitlab.app.com"
